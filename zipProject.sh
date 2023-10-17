e7b55beb5fafe3a2dce017a97b162c7e2f508c70
SebastiaoCarvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2023-10-17 18:21:28 +0100

    fix doping of character

--- zipProject.sh
+++ zipProject.sh
@@ -3,6 +3,6 @@ cp  -r Assets/ UnitySourceCode/
 cp  -r ProjectSettings/ UnitySourceCode/
 cp -r Packages/ UnitySourceCode/
 cp report/report.pdf ./report.pdf
-zip -r  P2Group04 UnitySourceCode/ report.pdf
+zip -r  Group04_IAJ-DecisionMakink UnitySourceCode/ report.pdf
 rm -rf UnitySourceCode
 rm report.pdf
