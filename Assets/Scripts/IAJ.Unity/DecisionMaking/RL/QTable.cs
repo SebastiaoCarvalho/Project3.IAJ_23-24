c3907497ee7525b65f8264b11fe02d8e5501c498
SebastiaoCarvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2023-10-27 15:27:37 +0100

    refactor some code in QLearning and add serialization

--- Assets/Scripts/IAJ.Unity/DecisionMaking/RL/QTable.cs
+++ Assets/Scripts/IAJ.Unity/DecisionMaking/RL/QTable.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Action = Assets.Scripts.IAJ.Unity.DecisionMaking.ForwardModel.Action;
 
 namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
 {
+    [Serializable]
     public class QTable
     {
-        private Dictionary<string, Dictionary<Action, float>> QValues { get; set; } 
-
+        private Dictionary<string, Dictionary<string, float>> QValues; /* Dictionary<stateName, Dictionary<actionName, QValue>> */
+        public Dictionary<string, Dictionary<string, float>> GetQValues() { return QValues; }
         public QTable()
         {
-            this.QValues = new Dictionary<string, Dictionary<Action, float>>();
+            this.QValues = new Dictionary<string, Dictionary<string, float>>();
+        }
+
+        public QTable(Dictionary<string, Dictionary<string, float>> qValues)
+        {
+            this.QValues = qValues;
         }
 
         // TODO: Initialize QTable
@@ -19,8 +26,8 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
         public float GetQValue(RLState state, Action action) {
             if (QValues.ContainsKey(state.ToString())) {
                 var subTable = QValues[state.ToString()];
-                if (subTable.ContainsKey(action)) {
-                    var QValue = subTable[action];
+                if (subTable.ContainsKey(action.Name)) {
+                    var QValue = subTable[action.Name];
                     return QValue;
                 }
                 else {
@@ -35,41 +42,46 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
         public void SetQValue(RLState state, Action action, float newValue) {
             if (QValues.ContainsKey(state.ToString())) {
                 var subTable = QValues[state.ToString()];
-                if (subTable.ContainsKey(action)) {
-                    subTable[action] = newValue;
+                if (subTable.ContainsKey(action.Name)) {
+                    subTable[action.Name] = newValue;
                 }
                 else {
-                    subTable.Add(action, newValue);
+                    subTable.Add(action.Name, newValue);
                 }
             }
             else {
                 Debug.Log("Create state " + state.ToString());
-                QValues.Add(state.ToString(), new Dictionary<Action, float>() { { action, newValue } });
+                QValues.Add(state.ToString(), new Dictionary<string, float>() { { action.Name, newValue } });
             }
         }
 
         public Action GetBestAction(RLState state) {
-            if (!QValues.ContainsKey(state.ToString())) {
-                Dictionary<Action, float> newSubTable = new Dictionary<Action, float>();
-                var actions = state.GetExecutableActions();
-                foreach (var action in actions) {
-                    newSubTable.Add(action, 0.0f);
-                }
-
-                QValues.Add(state.ToString(), newSubTable);
+            if (!QValues.ContainsKey(state.ToString())) { 
+                QValues.Add(state.ToString(), new Dictionary<string, float>());
             }
-
+            
             var subTable = QValues[state.ToString()];
-            Action bestAction = null;
+            var actions = state.GetExecutableActions();
+            foreach (var action in actions) {
+                if (!subTable.ContainsKey(action.Name))
+                    subTable.Add(action.Name, 0.0f);
+            }
+            for (int i = subTable.Count - 1; i >= 0; i--) {
+                string key = subTable.ElementAt(i).Key;
+                if (!actions.ToList().Exists(a => a.Name == key)) {
+                    subTable.Remove(key);
+                }
+            }
+            string bestAction = null;
             float bestValue = float.MinValue;
-            foreach (KeyValuePair<Action, float> kvp in subTable) {
-                if (kvp.Value > bestValue && kvp.Key.CanExecute()) {
+            foreach (KeyValuePair<string, float> kvp in subTable) {
+                if (kvp.Value > bestValue) {
                     bestAction = kvp.Key;
                     bestValue = kvp.Value;
                 }
             }
 
-            return bestAction;
+            return actions.ToList().Find(a => a.Name == bestAction);
         }
     }
 }
---
+++ Assets/Scripts/Serialization/QTableSerializer.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using System.IO;
+using Assets.Scripts.IAJ.Unity.DecisionMaking.RL;
+using UnityEngine;
+public class QTableSerializer
+{
+    public QTable qTable;
+    public string fileName = "qtable.json";
+
+    public void Save()
+    {
+        string json = SerializeQTable();
+        File.WriteAllText(Application.dataPath + "/" + fileName, json);
+    }
+
+    public void Load()
+    {
+        try {
+            string json = File.ReadAllText(Application.dataPath + "/" + fileName);
+            qTable = new QTable(DeserealizeQTable(json));
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogError("File not found : " + e.Message);
+        }
+    }
+
+    private string SerializeQTable() {
+        Dictionary<string, Dictionary<string, float>> qValues = qTable.GetQValues();
+        int tabCount = 1;
+        string json = "{\n";
+        foreach (KeyValuePair<string, Dictionary<string, float>> state in qValues) {
+            json += new string('\t', tabCount) + "\"" + state.Key + "\":{\n";
+            tabCount++;
+            foreach (KeyValuePair<string, float> action in state.Value) {
+                json += new string('\t', tabCount) + "\"" + action.Key + "\":" + action.Value + ",\n";
+            }
+            tabCount--;
+            json = json[..^2] + "\n";
+            json += new string('\t', tabCount) + "},\n";
+        }
+        json = json[..^2] + "\n";
+        json += "}";
+        return json;
+    }
+
+    private Dictionary<string, Dictionary<string, float>> DeserealizeQTable(string json) {
+        Dictionary<string, Dictionary<string, float>> qValues = new Dictionary<string, Dictionary<string, float>>();
+        string[] lines = json.Split('\n');
+        int tabCount = 0;
+        string state = "";
+        foreach (string line in lines) {
+            if (line.Contains("{")) {
+                tabCount++;
+                if (line != "{") {
+                    string trimmed = line.Trim();
+                    string key = trimmed.Substring(0, trimmed.Length - 2).Trim('"');
+                    state = key;
+                    qValues.Add(key, new Dictionary<string, float>());
+                }
+            }
+            else if (line.Contains("}")) {
+                tabCount--;
+            }
+            else {
+                string[] keyValue = line.Split(':');
+                if (keyValue.Length == 2) {
+                    if (keyValue[0].Contains("\"")) {
+                        string key = keyValue[0].Trim().Trim('"');
+                        if (tabCount == 2) {
+                            string action = key;
+                            float value = float.Parse(keyValue[1].Trim().Trim(','));
+                            qValues[state].Add(action, value);
+                        }
+                    }
+                }
+            }
+        }
+        return qValues;
+    }
+}
