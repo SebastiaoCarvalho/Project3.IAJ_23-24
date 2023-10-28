fb2a253b45cb1f50dc0385e3d3529a83e650b9df
Joao Vitor <joaobvitor@gmail.com>
2023-10-28 15:53:48 +0100

    Move previous state to rlstate and change rewards to be based on previous state

--- Assets/Scripts/IAJ.Unity/DecisionMaking/RL/QLearning.cs
+++ Assets/Scripts/IAJ.Unity/DecisionMaking/RL/QLearning.cs
@@ -13,8 +13,7 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
         public float TotalProcessingTime { get; set; }
         protected int CurrentIterations { get; set; }
         protected int FrameCurrentIterations { get; set; }
-        protected RLState PreviousState { get; set; }
-        public RLState CurrentState {  get; protected set; }
+        public RLState State {  get; protected set; }
         public QTable Store { get; protected set; }
         protected Action ExecutedAction { get; set; }
         //protected MCTSNode InitialNode { get; set; }
@@ -28,14 +27,14 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
         public QLearning(RLState initialState)
         {
             this.InProgress = false;
-            this.CurrentState = initialState;
+            this.State = initialState;
             this.MaxIterations = 1000;
             this.MaxIterationsPerFrame = 500;
             this.RandomGenerator = new System.Random();
             Store = new QTable();
             Alpha = 0.5f;
             Gamma = 0.1f;
-            Epsilon = 0.05f;
+            Epsilon = 0f;
             NewAction = false;
         }
 
@@ -45,16 +44,12 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
             this.CurrentIterations = 0;
             this.FrameCurrentIterations = 0;
             this.TotalProcessingTime = 0.0f;
-            this.PreviousState = null;
-        }
-
-        public void Reset() {
-            this.PreviousState = null;
+            this.State.Reset();
         }
 
         public Action ChooseAction()
         {
-            if (CurrentState.IsTerminal())
+            if (State.IsTerminal())
             {
                 return null;
             }
@@ -67,7 +62,7 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
             }
             */
             
-            Action[] executableActions = CurrentState.GetExecutableActions();
+            Action[] executableActions = State.GetExecutableActions();
             //actions = problem.getAvailableActions(state)
 
             InProgress = false;
@@ -76,43 +71,42 @@ namespace Assets.Scripts.IAJ.Unity.DecisionMaking.RL
             if (randomActionChance < Epsilon) //pick a random action every once in a while
             {
                 //action = actions.getRandomAction()
-                PreviousState = CurrentState.Copy();
-                ExecutedAction = CurrentState.GetRandomAction();
+                ExecutedAction = State.GetRandomAction();
                 return ExecutedAction;
             }
             else
             {
-                PreviousState = CurrentState.Copy();
-                ExecutedAction = Store.GetBestAction(CurrentState);
+                ExecutedAction = Store.GetBestAction(State);
                 return ExecutedAction;
             }
         }
 
         public void UpdateQTable()
         {
-            this.CurrentState.Initialize();
+            this.State.Initialize();
             this.InProgress = true;
-            if (!NewAction || PreviousState == null) {
+            if (!NewAction || State.PreviousState == null) {
                 return;
             }
 
             NewAction = false;
 
-            float reward = CurrentState.GetReward();
+            float reward = State.GetReward();
 
             //reward, newState = problem.performAction(state,action)
                 
             //Q = store.getQValue(state,action)
-            float Q = Store.GetQValue(PreviousState, ExecutedAction);
+            float Q = Store.GetQValue(State.PreviousState, ExecutedAction);
 
-            float newStateBestQ = Store.GetQValue(CurrentState, Store.GetBestAction(CurrentState));
+            float newStateBestQ = Store.GetQValue(State, Store.GetBestAction(State));
             //maxQ = store.getQValue(newState,store.getBestAction(newState))
             float newQ = (1 - Alpha) * Q + Alpha * (reward + Gamma * newStateBestQ);
             Debug.Log("Q : " + Q + " reward : " + reward + " newStateBestQ : " + newStateBestQ + " newQ : " + newQ);
-            Store.SetQValue(PreviousState, ExecutedAction, newQ);
+            Store.SetQValue(State.PreviousState, ExecutedAction, newQ);
                 
             //store.storeQValue(state,action,Q)
         }
+
         public void SaveQTable()
         {
             Debug.LogWarning("Saving QTable");
