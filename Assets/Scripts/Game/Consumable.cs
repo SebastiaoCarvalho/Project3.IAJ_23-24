6afde1da524a30bd247c6d8d5bca70819ad0523e
SebastiaoCarvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2023-10-29 14:32:59 +0000

    add restart to character and create consumable restart

---
+++ Assets/Scripts/Game/Consumable.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{    
+    public class Consumable : MonoBehaviour {
+        private Vector3 initialPosition;
+
+        void Awake()
+        {
+            initialPosition = this.transform.position;
+        }
+
+        public void Reset()
+        {
+            this.transform.position = initialPosition;
+        }
+    }
+}
